f82e328f7fefb9566195aff80e25f9984a083c73
Fumo <2278122422@qq.com>
2023-03-27 22:38:47 +0800

    main

--- src/hust/cs/javacourse/search/index/impl/Document.java
+++ src/hust/cs/javacourse/search/index/impl/Document.java
@@ -1,4 +1,111 @@
 package hust.cs.javacourse.search.index.impl;
 
-public class Document {
+import hust.cs.javacourse.search.index.AbstractDocument;
+import hust.cs.javacourse.search.index.AbstractTermTuple;
+
+import java.util.List;
+
+public class Document extends AbstractDocument {
+
+    /**
+     * 获得文档id
+     *
+     * @return ：文档id
+     */
+    @Override
+    public int getDocId() {
+        return 0;
+    }
+
+    /**
+     * 设置文档id
+     *
+     * @param docId ：文档id
+     */
+    @Override
+    public void setDocId(int docId) {
+
+    }
+
+    /**
+     * 获得文档绝对路径
+     *
+     * @return ：文档绝对路径
+     */
+    @Override
+    public String getDocPath() {
+        return null;
+    }
+
+    /**
+     * 设置文档绝对路径
+     *
+     * @param docPath ：文档绝对路径
+     */
+    @Override
+    public void setDocPath(String docPath) {
+
+    }
+
+    /**
+     * 获得文档包含的三元组列表
+     *
+     * @return ：文档包含的三元组列表
+     */
+    @Override
+    public List<AbstractTermTuple> getTuples() {
+        return null;
+    }
+
+    /**
+     * 向文档对象里添加三元组, 要求不能有内容重复的三元组
+     *
+     * @param tuple ：要添加的三元组
+     */
+    @Override
+    public void addTuple(AbstractTermTuple tuple) {
+
+    }
+
+    /**
+     * 判断是否包含指定的三元组
+     *
+     * @param tuple ： 指定的三元组
+     * @return ： 如果包含指定的三元组，返回true;否则返回false
+     */
+    @Override
+    public boolean contains(AbstractTermTuple tuple) {
+        return false;
+    }
+
+    /**
+     * 获得指定下标位置的三元组
+     *
+     * @param index ：指定下标位置
+     * @return：三元组
+     */
+    @Override
+    public AbstractTermTuple getTuple(int index) {
+        return null;
+    }
+
+    /**
+     * 返回文档对象包含的三元组的个数
+     *
+     * @return ：文档对象包含的三元组的个数
+     */
+    @Override
+    public int getTupleSize() {
+        return 0;
+    }
+
+    /**
+     * 获得Document的字符串表示
+     *
+     * @return ： Document的字符串表示
+     */
+    @Override
+    public String toString() {
+        return null;
+    }
 }
